edc7129f1da26f5eb388dd43b287ebe0c1734b13
emarkose3 <46685714+emarkose3@users.noreply.github.com>
2019-01-14 13:30:48 -0500

    Add files via upload

---
+++ hwtest.scala
@@ -0,0 +1,17 @@
+
+object hw1 extends eecs.cs384 {
+
+  def userName = "Elias Markose"
+
+  def sum(numbers: Array[Int]): Int = {
+    ???
+  }
+  test("sum", sum _, "numbers")
+
+// Assume that the Array has at least 1 element
+  def min(numbers: Array[Int]): Int = {
+    ???
+  }
+  test("min", min _, "numbers")
+
+} //end of hw1
